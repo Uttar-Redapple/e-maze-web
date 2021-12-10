4c8099a898f360915ee4059cc71d193c52871b2e
Arkadip-Redapple <69346352+Arkadip-Redapple@users.noreply.github.com>
2021-12-10 20:04:26 +0530

    abd

--- web-build/flutter_service_worker.js
+++ web-build/flutter_service_worker.js
@@ -205,9 +205,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "7d5d0061dd7483ac47d029666c300850",
-"/": "7d5d0061dd7483ac47d029666c300850",
-"main.dart.js": "e4fccb7459bf4e3f6cfbfd1d392e1f1a",
+"index.html": "eff95247dce08752794139039228dcfb",
+"/": "eff95247dce08752794139039228dcfb",
+"main.dart.js": "9e292a27a63a416aee90834d35c92647",
 "manifest.json": "c5579b7aad76a50cfdb9a92e58476fa0",
 "version.json": "21eb5b0e6da402b746c863119972a629"
 };
